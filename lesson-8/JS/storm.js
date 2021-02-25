1cc5f9cf1324e4953b15f32e0a4f3ab12bf82f22
sbed13@byui.edu <sbed13@byui.edu>
2021-02-25 10:49:06 -0600

    fix menu js link

---
+++ lesson-8/JS/storm.js
@@ -0,0 +1,13 @@
+function toggleMenu(){
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+ }
+
+
+ WebFont.load({
+    google: {
+    families: [
+       'Yusei Magic'
+    ]
+    }
+    });
