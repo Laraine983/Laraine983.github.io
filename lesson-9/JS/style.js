6544ef3783f6fb19e2bca13f3537177860394e80
sbed13@byui.edu <sbed13@byui.edu>
2021-03-02 20:47:01 -0600

    commit

---
+++ lesson-9/JS/style.js
@@ -0,0 +1,15 @@
+
+
+function toggleMenu(){
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+ }
+
+
+ WebFont.load({
+    google: {
+    families: [
+       'Yusei Magic'
+    ]
+    }
+    });
