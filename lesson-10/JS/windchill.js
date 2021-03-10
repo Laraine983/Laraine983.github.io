8aafbb3202ceac6787f8fc3372a0655e46cdce9f
sbed13@byui.edu <sbed13@byui.edu>
2021-03-10 16:45:29 -0600

    winchill

--- lesson-10/JS/windchill.js
+++ lesson-10/JS/windchill.js
@@ -1,11 +1,9 @@
 
 
- 
-
 function windChill(temperature, windSpeed){                 /* need to define temp and windspeed, try debugging again*/
 
-   let t = parseFloat(document.getElementById("temperature").innerHTML);
-   let s = parseFloat(document.getElementById("windSpeed").innerHTML);
+   let temperature = parseFloat(document.getElementById("temperature").innerHTML);
+   let windSpeed = parseFloat(document.getElementById("windSpeed").innerHTML);
    
    if (temperature <= 50 && windSpeed >= 3) {
       f= 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
