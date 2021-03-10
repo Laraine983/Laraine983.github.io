71a881bac4eeaf9fae54c9bda39de155d115052a
sbed13@byui.edu <sbed13@byui.edu>
2021-03-10 15:37:44 -0600

    add images

---
+++ lesson-10/JS/windchill.js
@@ -0,0 +1,20 @@
+
+
+ 
+
+function windChill(temperature, windSpeed){                 /* need to define temp and windspeed, try debugging again*/
+
+   let t = parseFloat(document.getElementById("temperature").innerHTML);
+   let s = parseFloat(document.getElementById("windSpeed").innerHTML);
+   
+   if (temperature <= 50 && windSpeed >= 3) {
+      f= 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
+      return f;
+   }
+   else{
+      return "N/A";
+   }
+}
+   document.getElementById("windChill").innerHTML = windChill();
+
+
