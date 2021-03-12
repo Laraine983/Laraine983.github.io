abad602cbd0d7ab81fedf79a67c1c6d59106b519
sbed13@byui.edu <sbed13@byui.edu>
2021-03-12 14:36:40 -0600

    start forecast css

--- lesson-10/JS/prestonapi.js
+++ lesson-10/JS/prestonapi.js
@@ -9,14 +9,14 @@
  .then((jsObject) => {
    console.log(jsObject);
    
-    document.getElementById('current-temperature').innerHTML = jsObject.main.temp; //get current temp
-    document.getElementById('temperature').innerHTML = jsObject.main.temp_max;  //get high temp
-    document.getElementById('low-temperature').innerHTML = jsObject.main.temp_min; //get low temp
-    document.getElementById('humidity').innerHTML = jsObject.main.humidity; //get humidity %
-    document.getElementById('windSpeed').innerHTML = jsObject.wind.speed; //get wind speed
+    document.getElementById('current-temperature').innerHTML = Math.round(jsObject.main.temp); //get current temp
+    document.getElementById('temperature').innerHTML = Math.round(jsObject.main.temp_max);  //get high temp
+    document.getElementById('low-temperature').innerHTML = Math.round(jsObject.main.temp_min); //get low temp
+    document.getElementById('humidity').innerHTML = Math.round(jsObject.main.humidity); //get humidity %
+    document.getElementById('windSpeed').innerHTML = Math.round(jsObject.wind.speed); //get wind speed
 
 
-  
+  //add windchill function
     let temperature = parseFloat(document.getElementById("temperature").innerHTML);
     let windSpeed = parseFloat(document.getElementById("windSpeed").innerHTML);
    let windchill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
@@ -43,24 +43,23 @@ fetch(apiForecastURL)
   
 
   let day = 0;
-  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const forecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];                //set days of the week
+  const forecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));         //filter to specific time of day
 
   forecast.forEach(x => {
     
 
       let f = new Date(x.dt_txt);
       
-      document.getElementById(`dayofweek${day+1}`).textContent = weekdays[f.getDay()];
-      document.getElementById(`temperature${day+1}`).textContent = Math.round(x.main.temp);
-      document.getElementById(`icon${day+1}`).textContent = x.weather[0].description;
+      document.getElementById(`dayofweek${day+1}`).textContent = weekdays[f.getDay()];           //get day of the week and display on page
+      document.getElementById(`temperature${day+1}`).textContent = Math.round(x.main.temp);     //get temp and display as whole number
+     
 
-      const imagesrc = 'https://openweathermap.org/img/w/' + x.weather[0].icon + '.png';
-      const imagedesc = x.weather[0].description;
+      let imagesrc = 'https://openweathermap.org/img/w/' + x.weather[0].icon + '.png';        //define image source
+      let imagedesc = x.weather[0].description;                                               //define image alt text
 
-
-      document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
-      document.getElementById(`icon${day+1}`).setAttribute('alt', imagedesc);
+      document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);                  //set weather icon to image source 
+      document.getElementById(`icon${day+1}`).setAttribute('alt', imagedesc);                 //set alt text to icon
 
       day++;
   });
