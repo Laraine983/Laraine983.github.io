88ed3b72add0cf22d4d2c7131bc856368c08c2e4
sbed13@byui.edu <sbed13@byui.edu>
2021-03-16 16:13:00 -0500

    combine JS add imgs

--- lesson-11/JS/script.js
+++ lesson-11/JS/script.js
@@ -4,8 +4,7 @@ function toggleMenu(){
 }
 
 
-
-
+//home page
     
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';     // URL to the JSON file 
 
@@ -50,11 +49,87 @@ fetch(requestURL)
   }
 }
   });
-  
-  WebFont.load({
-    google: {
-    families: [
-       'Yusei Magic'
-    ]
+
+
+  //storm center slider
+  function adjustRating(rating) {               
+    document.getElementById("ratingAmt").innerHTML = rating;     /*JS from example given by Jason Blazzard*/
+ }
+ 
+
+ //gallery page
+
+/* Original Creator: Kevin Powell  https://www.youtube.com/watch?v=mC93zsEsSrg&feature=emb_logo */
+
+const images = document.querySelectorAll("[data-src]");  /*pulls items with specific [data-src] attribute*/
+
+function preloadImage(img){ /* function searches for img */
+    const src = img.getAttribute("data-src"); /* finds data-src attribute within img */
+    if(!src){        /* if no data-src found, return. */
+        return;
     }
-    });
+    img.src = src;   /* If data-src is found, const src will make data-src become src (make image appear) */ 
+    img.removeAttribute("data-src") ; 
+}
+
+const imgOptions = {
+    threshold: 1,                       /* higher threshold means slower loading time of images*/
+    rootMargin: " 0px 0px -100px 0px",  /* -100 loads image when image is 100px into the viewport. Positive 100 would load 100px before viewport*/
+                                        /* Can use different numbers, i.e. -500, -400, -300...300, 400, 500 px */
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) =>{
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){    /* if entry is not intersecting, return. Else, load image */
+            return;
+        } else{
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target); /* once image is preloaded, stop observing it with unobserve */
+        }
+    })
+
+}, imgOptions);
+
+images.forEach(image => {           /* cannot apply one imgObserver to entire list of images, 
+                                    we need to run imgObserver for each image inside of images list (observe each image individually)*/ 
+    imgObserver.observe(image);
+});
+
+//load fonts
+WebFont.load({
+  google: {
+  families: [
+     'Yusei Magic'
+  ]
+  }
+  });
+
+
+//friday banner
+  let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  let d = new Date();
+  let day = d.getDay(); 
+  let date = d.getDate();
+  
+  if (weekDays[day] == 'Friday') {
+      document.getElementById("friday-banner").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+      document.getElementById("friday-banner").style.display = "block";
+  }
+  else {
+      document.getElementById("friday-banner").style.display = "none";
+  }
+
+
+
+
+//get date - footer
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+
+
+
+
+
+ 
+ 
+
