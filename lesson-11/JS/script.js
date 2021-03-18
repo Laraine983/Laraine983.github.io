139c864be6dd44e9f0261df0125cf2e7e28c5ade
sbed13@byui.edu <sbed13@byui.edu>
2021-03-18 15:56:43 -0500

    js

--- lesson-11/JS/script.js
+++ lesson-11/JS/script.js
@@ -119,14 +119,7 @@ images.forEach(image => {           /* cannot apply one imgObserver to entire li
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
 
-//load fonts
-WebFont.load({
-  google: {
-  families: [
-     'Yusei Magic'
-  ]
-  }
-  });
+
 
 
 
