125552c15682566eaf5a64488a85cedf548db904
sbed13@byui.edu <sbed13@byui.edu>
2021-03-31 19:22:08 -0500

    home paragraphs css, contact info

--- lesson-12/js/3day.js
+++ lesson-12/js/3day.js
@@ -1,7 +1,7 @@
 
 
 
-// API key: 0bc9f4baea7b9d07d7e6fa8b23b2f5f6
+// My API key: 0bc9f4baea7b9d07d7e6fa8b23b2f5f6
 
 
 const forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.2148&lon=-97.1331&units=imperial&appid=0bc9f4baea7b9d07d7e6fa8b23b2f5f6";
