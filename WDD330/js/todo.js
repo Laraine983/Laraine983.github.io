4e9a83d2cc360db277cea4b1029f039ff74890f2
sbed13@byui.edu <sbed13@byui.edu>
2021-10-19 13:10:20 -0500

    start todo js

--- WDD330/js/todo.js
+++ WDD330/js/todo.js
@@ -1,1 +1,16 @@
 
+function addTask(){
+    var p = document.createElement("p");  
+    var input = document.getElementById("inputBox");
+    p.innerHTML = input.value;
+    input.value = "";
+
+    document.getElementById("taskList").appendChild(p);
+}
+
+/*
+function removeItem(){
+    let item = document.getElementById("deleteMe");
+    item.remove();
+}
+*/
