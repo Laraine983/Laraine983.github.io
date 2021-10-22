a779128ed0aaa5e183791498126dbb139800d205
sbed13@byui.edu <sbed13@byui.edu>
2021-10-22 18:54:33 -0500

    fix js buttons

--- WDD330/js/todo.js
+++ WDD330/js/todo.js
@@ -1,87 +1,68 @@
+const list = document.querySelector('ul');
+const complete = document.getElementsByClassName('completedButton');
 
-
-var taskList = document.getElementsByTagName("li");
-var i;
-for (i = 0; i < taskList.length; i++) {
-  var div = document.createElement("div");
-  
-  div.className = "close";
-  div.appendChild(txt);
-  taskList[i].appendChild(div);
-}
-
-
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'li') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var div = document.createElement("div");
-  var txt = document.createTextNode("x");
-  div.className = "close";
-  div.appendChild(txt);
-  li.appendChild(div);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-}
-
-
-
-/*
-function addTask(){
-    var p = document.createElement("p");  
-    p.setAttribute("id", "taskItem");
-    var input = document.getElementById("inputBox");
-    p.innerHTML = 
+function addListItem(){
+    var item = document.createElement("li");  
+    item.setAttribute("class", "taskItem");
+    var input = document.getElementById("myInput");
+    item.innerHTML = 
     `<label class="checkContainer">
-    <input id="checkmark" class="checkmark done" type="checkbox" >
-    <div class="checkmark"></div>
+    <input id="input" type="checkbox" >
     </label>`
    + input.value + 
-    `<button id="devare" onclick="removeItem()">X</button>`;
+    `<button class="deleteBtn">X</button>`;
     
-
-    document.getElementById("taskList").appendChild(p);
-    document.getElementById("inputBox").value = '';
+    document.getElementById("myUL").appendChild(item);
+    document.getElementById("myInput").value = '';
 }
 
-function removeItem() {
-    var devareItem = document.getElementById("taskItem");
-    devareItem.remove();
-    }
 
+//delete list item 
+list.addEventListener('click', (e) => {
+  if (e.target.classList.contains('deleteBtn') === true) {
+    e.target.parentElement.remove();
+    }
+});
+
+
+
+//not working?
+function checkItems() {
+var v = document.getElementsByClassName("taskItem");
+  if (document.querySelectorAll('input[type=checkbox]:checked')) {
+            v.classList.add("checkMarked");
+        } 
+
+  //change to for loops
+}
+  function completedTasks() {
+var v = document.getElementsByClassName("taskItem");
+  if (v.classList == "checkMarked") {
+            v[0].style.display = 'block';
+        } 
+else{
+  v[0].style.display = 'none';
+}
+}
   
-*/
+
+
+function activeTasks(){
+  var v = document.getElementsByClassName("taskItem");
+  if (v.classList !== "checkMarked") {
+            v[0].style.display = 'block';
+        } 
+else{
+  v[0].style.display = 'none';
+}}
+  
+
+
+
+function selectAll(){
+  var v = document.getElementsByClassName("taskItem");
+  v[0].style.display = 'block';
+}
 
 
 
