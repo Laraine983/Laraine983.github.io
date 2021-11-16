2d0ccf4a481f85a295616722c106bda5644d21a7
sbed13@byui.edu <sbed13@byui.edu>
2021-11-16 14:30:09 -0600

    f

---
+++ WDD330/Final Project/main.js
@@ -0,0 +1,6 @@
+//cat api api key: fe962b92-42b3-407d-a606-5ef01fc37aa7
+//petfinder api key: xwPTzdBO8s90UmgqV0AkJ83ieYQfjSSFc41SLWmouxNOqfUcxr
+//petfinder secret: DEKXDEjqwstYy5MHM3UEPE98MzGjrHpxEI3q03sJ
+
+const apiKey = 'xwPTzdBO8s90UmgqV0AkJ83ieYQfjSSFc41SLWmouxNOqfUcxr'; // assign our key to a variable, easier to read
+
