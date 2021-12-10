e5e006a6bf4d315294ae6e82abc4217756cd488b
sbed13@byui.edu <sbed13@byui.edu>
2021-12-10 15:29:14 -0600

    fetch json

--- WDD330/Final Project/js/dogsData.js
+++ WDD330/Final Project/js/dogsData.js
@@ -1,14 +1,14 @@
 
 //put this into json file and fetch instead
-fetch("data.json")
-  .then(response => response.json())
-  .then(json => console.log(json));
-/*
-function change() {
+
+//let mainObj = {};
+//let dogsData = [];
+
+let showObj = function(){
   var e = document.querySelector("select.breeds");
   var strUser = e.options[e.selectedIndex].text;
   console.log(strUser);
-let dogsData = json.dogsData;
+ let dogsData = mainObj["dogsData"];
   for (let i = 0; i < dogsData.length; i++) {
     if (strUser === dogsData[i].breed) {
       document.querySelector("h2.breedName").textContent = dogsData[i].breed;
@@ -23,4 +23,23 @@ let dogsData = json.dogsData;
     }
   }
 }
-*/
+
+
+fetch("./js/data.json")
+ .then(function(resp){
+   return resp.json();
+ })
+ .then(function(data){
+   console.log(data);
+   mainObj = data;
+   //dogsData = data.dogsData;
+   showObj();
+   
+ });
+
+
+
+
+
+
+
