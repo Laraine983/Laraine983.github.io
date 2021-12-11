b934648bcf53202838591c35d929ff651ecbdc7e
sbed13@byui.edu <sbed13@byui.edu>
2021-12-11 17:57:37 -0600

    audio

--- WDD330/Final Project/js/audio.js
+++ WDD330/Final Project/js/audio.js
@@ -12,23 +12,26 @@ barkAudio.setAttribute('src', './audio/akita.mp3')
 */
 
 
- document.querySelector("select.breeds").onchange = function() {
+ document.getElementById("tryAudio").onchange = function() {
 var e = document.querySelector("select.breeds")
 var strUser = e.options[e.selectedIndex].text;
 console.log(strUser);
-//let dogsData = e["dogsData"];
+
 for (let i = 0; i < strUser.length; i++) {
- // if (strUser === dogsData[i].breed) {
+ 
     let barkAudio = document.getElementById('bark');
     barkAudio.setAttribute('src', './audio/'+ `${strUser}` +'.mp3')
-    console.log(barkAudio);
+   
 
     document.getElementById("doggo").onclick = function() {
       var audio = document.getElementById("bark");
       if (audio.paused) audio.play();
       else audio.pause();
   };
+  
+
   }
+ 
 }
 
 
